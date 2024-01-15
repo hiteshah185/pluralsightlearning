fbaa5ce2be447933811501b186394d49e41585d0
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-01-15 12:38:55 +0530

    Collection - Iterator methods and Examples

--- JavaCollecctions/FundamentalsOfCollections/src/com/teranet/rps/learningcollections/workingWithCollections/CollectionConcepts.java
+++ JavaCollecctions/FundamentalsOfCollections/src/com/teranet/rps/learningcollections/workingWithCollections/CollectionConcepts.java
@@ -4,6 +4,8 @@ import com.teranet.rps.learningcollections.models.FlipkartProduct;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class CollectionConcepts {
     public static void main(String[] args) {
@@ -19,10 +21,43 @@ public class CollectionConcepts {
         Collection<FlipkartProduct> products = new ArrayList<>();
         products.add(phone);
         products.add(tv);
+        products.add(book);
 
         for(FlipkartProduct item:products){
             System.out.println(item.toString());
         }
+
+        // Refactor to use iterator to remove products.
+        Iterator<FlipkartProduct> iterator = products.iterator();
+        while (iterator.hasNext()){
+            final FlipkartProduct product = iterator.next();
+            if(product.getPrice()>30000){
+                iterator.remove();
+            }
+        }
+        System.out.println(products);
+
+        System.out.println(products.size());
+        System.out.println(products.isEmpty());
+        System.out.println(products.contains(book));
+        products.remove(phone);
+
+        System.out.println(products);
+
+        List<FlipkartProduct> flipkartProductList = new ArrayList<>();
+        flipkartProductList.add(book);
+        flipkartProductList.add(phone);
+        for(FlipkartProduct prod:flipkartProductList){
+            System.out.println(prod.getProductName());
+        }
+        flipkartProductList.removeAll(products);
+        System.out.println("List after removing using a collection List:");
+        for(FlipkartProduct prod:flipkartProductList){
+            System.out.println(prod.getProductName());
+        }
+
+
+
 
     }
 }

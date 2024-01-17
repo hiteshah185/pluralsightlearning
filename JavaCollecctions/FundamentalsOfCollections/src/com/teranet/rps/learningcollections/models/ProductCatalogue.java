47ec5cb31cc0eb483f91d572f14da0845e5b38e8
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-01-17 11:44:56 +0530

    Collections Sets example - use case

---
+++ JavaCollecctions/FundamentalsOfCollections/src/com/teranet/rps/learningcollections/models/ProductCatalogue.java
@@ -0,0 +1,17 @@
+package com.teranet.rps.learningcollections.models;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ProductCatalogue implements Iterable<FlipkartProduct>{
+    private final Set<FlipkartProduct> products = new HashSet<>();
+    public void addSupplier(final Supplier supplier){
+        products.addAll(supplier.getProducts());
+    }
+    @Override
+    public Iterator<FlipkartProduct> iterator() {
+        return null;
+    }
+}

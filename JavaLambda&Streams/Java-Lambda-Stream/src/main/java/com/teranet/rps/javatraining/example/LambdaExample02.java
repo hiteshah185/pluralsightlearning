cc43e719f2756da00e6717fa23aa8191e5d4b78c
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-01-18 10:16:24 +0530

    List Sorting,Stream & Parallel Stream Example

---
+++ JavaLambda&Streams/Java-Lambda-Stream/src/main/java/com/teranet/rps/javatraining/example/LambdaExample02.java
@@ -0,0 +1,44 @@
+package com.teranet.rps.javatraining.example;
+
+import com.teranet.rps.javatraining.models.Employee;
+import com.teranet.rps.javatraining.models.SampleData;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaExample02 {
+    public static void main(String[] args) {
+        List<Employee> employeeList = SampleData.getEmployees();
+        BigDecimal yearOfExperienceLimit = new BigDecimal("2");
+        printEmployee(employeeList,yearOfExperienceLimit);
+        System.out.println();
+        employeeList.forEach(employee -> {
+            if(employee.getYearOfExperience().compareTo(yearOfExperienceLimit)<0){
+                System.out.println(employee.getName());
+            }
+        });
+        System.out.println();
+        var filterEmployees = employeeList.stream()
+                .filter(employee -> employee.getYearOfExperience().compareTo(yearOfExperienceLimit)<0)
+                .collect(Collectors.toList());
+        for(Employee employee:filterEmployees){
+            System.out.println(employee.getName());
+        }
+        System.out.println();
+        employeeList.parallelStream()
+                .forEach(employee -> {
+                    if(employee.getYearOfExperience().compareTo(yearOfExperienceLimit)<0){
+                        System.out.println(employee.getName());
+                    }
+                });
+
+    }
+    static void printEmployee(List<Employee> employees, BigDecimal yearOfExperience){
+        for(Employee employee: employees){
+            if(employee.getYearOfExperience().compareTo(yearOfExperience)<0){
+                System.out.println(employee.getName());
+            }
+        }
+    }
+}

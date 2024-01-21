fba351450624c8d69a68f90049d3ed4333ff8c79
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-01-21 21:39:30 +0530

    Java I/O - FileWriter

--- JavaIO/Input-Output-API/src/com/teranet/rps/java/StringWriter01.java
+++ JavaIO/Input-Output-API/src/com/teranet/rps/java/StringWriter01.java
@@ -1,2 +1,17 @@
-package com.teranet.rps.java;public class StringWriter01 {
+package com.teranet.rps.java;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+public class StringWriter01 {
+    public static void main(String[] args) {
+        try(Writer writer = new FileWriter("SampleFiles/Notepad.txt");){
+            writer.write("Hello Trivandrum from Teraner-TRV - RPS Team");
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
 }

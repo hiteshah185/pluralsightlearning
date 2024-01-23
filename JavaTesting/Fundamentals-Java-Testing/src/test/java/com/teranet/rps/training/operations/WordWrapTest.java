dc24f04c413d4df19e9b43c1010a92f54dfae995
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-01-23 15:36:57 +0530

    Java Testing - WordWrap unit Test

--- JavaTesting/Fundamentals-Java-Testing/src/test/java/com/teranet/rps/training/operations/WordWrapTest.java
+++ JavaTesting/Fundamentals-Java-Testing/src/test/java/com/teranet/rps/training/operations/WordWrapTest.java
@@ -1,2 +1,28 @@
-package com.teranet.rps.training.operations;public class WordWrapTest {
+package com.teranet.rps.training.operations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordWrapTest {
+    public static final int LINE_LENGTH = 6;
+    @Test
+    public void lineShouldWrapIfOverLineLength(){
+        //Two wrapping points
+        final String result = WordWrap.wrap("The modern India",LINE_LENGTH);
+        assertEquals("The mo\ndern I\nndia",result);
+    }
+    @Test
+    public void shortLinesNotWrapping(){
+        //No wrapping points
+        final String result= WordWrap.wrap("TRV",LINE_LENGTH);
+        assertEquals("TRV",result);
+    }
+
+    @Test
+    public void isMultipleWrappingPoints(){
+        final String result = WordWrap.wrap("Emerald backwaters, spice-kissed air, beaches serenaded by Arabian whispers",LINE_LENGTH);
+        assertEquals("Emeral\nd back\nwaters\n, spic\ne-kiss\ned air\n, beac\nhes se\nrenade\nd by A\nrabian\n whisp\ners",result);
+    }
+
 }

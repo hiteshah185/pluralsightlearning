cbf730b3bcde59f0284d2feddc1e90e8e55bba35
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-01-24 16:04:30 +0530

    Java JUnit Testing - Test Disabled

--- JavaTesting/Fundamentals-Java-Testing/src/test/java/com/teranet/rps/training/music/utils/DurationParserTest.java
+++ JavaTesting/Fundamentals-Java-Testing/src/test/java/com/teranet/rps/training/music/utils/DurationParserTest.java
@@ -1,11 +1,13 @@
 package com.teranet.rps.training.music.utils;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.Assert.assertEquals;
 
 public class DurationParserTest {
+    @Disabled("Avoided because of string parsing")
     @ParameterizedTest
     @CsvSource({"2 weeks, 14", "1 month, 30", "5 days, 5", "once, 1"})
     public void parseDurationWithValidUnitAndQuantity(String durationString, int expectedDays) {
